8c63ce6d59393fc788391f9f1458a07920df7a1e
Pete Ratzlaff <pratzlaff@cfa.harvard.edu>
2025-06-09 16:22:00 -0400

    N/A

--- src/process_archive_parallel.sh
+++ src/process_archive_parallel.sh
@@ -1,26 +1,22 @@
 #! /bin/bash
 
+set -eo pipefail
+
 [ $# -eq 0 ] || {
   \echo "Usage: $0" 1>&2
   exit 1
 }
 
-n=13
 
-set -e
-set -o pipefail
+n=13
+n=4
 
 . ~/python3_venv/bin/activate
 tmpfile=$(mktemp)
 #python3 /data/legs/rpete/flight/hrc_archive/src/obsids.py > "$tmpfile"
-cp -a /data/legs/rpete/flight/hrc_archive/obsids_todo "$tmpfile"
-outdir=/data/loss/rpete/hrc
-
-cp -a /data/legs/rpete/flight/hrc_archive/obsids_tmp "$tmpfile"
-outdir=/data/loss/rpete/hrc2
 
 cp -a /data/legs/rpete/flight/hrc_archive/obsids_hz43 "$tmpfile"
-outdir=/data/loss/rpete/hz43_patch_hrc_ssc_100000
+outdir=/data/loss/rpete/hz43_patch_hrc_ssc
 
 script=/data/legs/rpete/flight/hrc_archive/src/process_archive.sh
 
